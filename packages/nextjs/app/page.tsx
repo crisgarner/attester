dd324a6e53b55f4c912959df46c16acf50f9b2f6
Crisgarner <@crisgarner>
2024-04-06 10:25:59 -0600

    added new schema

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
   const [receiverAddress, setReceiverAddress] = useState("0x0000000000000000000000000000000000000000");
   const [openQr, setOpenQr] = useState<boolean>(false);
-  const schemaUID = "0xd465df4ee00a2176cff40e5e23226afc3acdd7871f3fa7a4b1c2c28ade7a30cf";
+  const schemaUID = "0x3f60dfeb9434008892fd3bf2470126cc44930a688ec7231168948efb2b1aa6f9";
   const [event, setEvent] = useState(schemas.events[0]);
 
   const handleEventChange = (e: any) => {
@@ -59,6 +59,7 @@ const Home: NextPage = () => {
       const encodedData = schemaEncoder.encodeData([
         { name: "id", value: event.id, type: "uint256" },
         { name: "name", value: event.name, type: "string" },
+        { name: "description", value: event.description, type: "string" },
         { name: "location", value: event.location, type: "string" },
         { name: "startDate", value: event.startDate, type: "string" },
         { name: "endDate", value: event.endDate, type: "string" },
@@ -189,7 +190,18 @@ const Home: NextPage = () => {
                   </div> */}
                 </div>
               </form>
-
+              <div>
+                <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Description
+                </label>
+                <textarea
+                  id="description"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4"
+                  placeholder="Ethereum TGU Meetup"
+                  value={event.description}
+                  disabled
+                />
+              </div>
               <button
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 onClick={onAttest}
